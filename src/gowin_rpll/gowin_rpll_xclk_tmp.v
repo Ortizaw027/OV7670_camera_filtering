548b5c74ba269412613146bcfb577dbc7d87ab44
Antonio Ortiz <Ortizaw027@gmail.com>
2025-04-13 17:56:57 -0400

    Add files via upload

---
+++ src/gowin_rpll/gowin_rpll_xclk_tmp.v
@@ -0,0 +1,20 @@
+//Copyright (C)2014-2025 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//Tool Version: V1.9.11.01 (64-bit)
+//Part Number: GW1NR-LV9QN88PC6/I5
+//Device: GW1NR-9
+//Device Version: C
+//Created Time: Sun Apr 13 16:41:47 2025
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+    Gowin_rPLL_XCLK your_instance_name(
+        .clkout(clkout), //output clkout
+        .lock(lock), //output lock
+        .reset(reset), //input reset
+        .clkin(clkin) //input clkin
+    );
+
+//--------Copy end-------------------
